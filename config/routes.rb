228c95904a838e07ec021d2f0a7dbb31169a4c25
Ricky Hurtado <rickyhurtado@users.noreply.github.com>
2016-12-30 22:25:36 -0500

    Move the about-this-app url segment to module's path

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'about-this-app', to: 'static_pages#about'
 
-  namespace :about, path: '' do
-    get 'about-this-app/greetings', to: 'greetings#index', as: 'greetings'
-    get 'about-this-app/greetings/:greet_me', to: 'greetings#greet_me', as: 'greet_me'
+  namespace :about, path: 'about-this-app' do
+    get 'greetings', to: 'greetings#index', as: 'greetings'
+    get 'greetings/:greet_me', to: 'greetings#greet_me', as: 'greet_me'
   end
 
   root 'static_pages#home'
