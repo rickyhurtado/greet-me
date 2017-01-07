9bc1c47b35ea7519c08adebafea83d700d20fbbb
Ricky Hurtado <odatruh.ykcir@gmail.com>
2017-01-07 16:18:40 +0800

    Modify the contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,9 @@
 class Contact < ApplicationRecord
+
+  scope :gender, -> (gender) { where(gender: gender) }
+  scope :males, -> { gender('m') }
+  scope :females, -> do
+    gender('f')
+  end
+
 end
