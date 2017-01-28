d45a369e8b31580c99906ee8d4f1b847106f4930
Ricky Hurtado <odatruh.ykcir@gmail.com>
2017-01-28 18:20:15 +0800

    Save new contact (create action)

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -11,5 +11,24 @@ module Admin
 
       render locals: { contact: contact }
     end
+
+    def create
+      contact = Contact.new(contact_params)
+
+      # See validations on person model
+      if contact.save
+        flash[:success_message] = "New contact has been created. <a href=\"#{ edit_admin_contact_url(contact)}\">View this contact.</a>"
+        redirect_to new_admin_contact_url
+      else
+        flash.now[:error] = contact.errors
+        render :new, locals: { contact: contact }
+      end
+    end
+
+    private
+
+      def contact_params
+        params.require(:contact).permit(:email, :full_name)
+      end
   end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,8 @@
 class Contact < ApplicationRecord
 
+  validates :full_name, :email, presence: true
+  validates_uniqueness_of :email
+
   has_and_belongs_to_many :interests
 
   scope :gender, -> (gender) { where(gender: gender) }
