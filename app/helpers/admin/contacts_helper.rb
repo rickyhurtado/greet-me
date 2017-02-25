c170ca22e3d9a19a40e3eb440b35470908a078fc
Ricky Hurtado <odatruh.ykcir@gmail.com>
2017-02-25 15:17:22 +0800

    Update the contact list in admin helper

--- app/helpers/admin/contacts_helper.rb
+++ app/helpers/admin/contacts_helper.rb
@@ -8,10 +8,7 @@ module Admin::ContactsHelper
   def contact_list(contacts)
     contacts.each do |contact|
       haml_tag :li do
-        haml_tag :a, contact.full_name, href: url_for(edit_admin_contact_url(contact))
-        haml_tag :span, ' [ '
-        haml_tag :a, 'delete', href: url_for(admin_contact_url(contact)), data: { method: :delete, confirm: 'Delete this contact?' }
-        haml_tag :span, ' ]'
+        haml_tag :a, contact.full_name, href: url_for(admin_contact_url(contact.id))
       end
     end
   end
